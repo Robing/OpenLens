e2ce6e7cef53a6f49e703ab3ef1f17fb7bd02e9e
Robing <yugege0@gmail.com>
2023-08-15 13:31:20 +0800

    Update update.js

--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const packageJsonOpenLens = require('./OpenLens/package.json');
+const packageJsonOpenLens = require('./lens/open-lens/package.json');
 
 packageJsonOpenLens.build.publish = [{
     url: "https://github.com/Robing/OpenLens/releases/download/Latest",
